eeca6af3a0dbbeafd973844c1ee576984143fafd
OELS <oelsart@gmail.com>
2025-02-16 19:09:51 +0900

    Gunplayとの競合を解決

---
+++ Source/VehicleInteriorFramework/VMF_HarmonyPatches/Patches_Gunplay.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace VehicleInteriors.VMF_HarmonyPatches
+{
+    public static class Patches_Gunplay
+    {
+        static Patches_Gunplay()
+        {
+            if (ModsConfig.IsActive("automatic.gunplay"))
+            {
+                VMF_Harmony.Instance.PatchCategory("VMF_Patches_Gunplay");
+            }
+        }
+    }
+
+    [HarmonyPatch("Gunplay.Patch.PatchProjectileLaunch", "Postfix")]
+    public static class Patch_PatchProjectileLaunch_Postfix
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.MethodReplacer(MethodInfoCache.g_Thing_Map, MethodInfoCache.m_BaseMap_Thing);
+        }
+    }
+}
