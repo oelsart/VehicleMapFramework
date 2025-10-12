ed0b9996b932b2227cafaafc53f40969b2db1a19
oelsart <oelsart@gmail.com>
2025-10-12 09:04:50 +0900

    VehicleTurretの一部virtual化に合わせ機能をパッチからVehicleturret_Manualに担わせる

---
+++ Source/VehicleMapFramework/Combat/VehicleTurret_Manual.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Vehicles;
+using Verse;
+
+namespace VehicleMapFramework;
+
+public class VehicleTurret_Manual : VehicleTurret
+{
+    public override void RecacheMannedStatus()
+    {
+        if (VehicleMod.settings.debug.debugShootAnyTurret)
+        {
+            IsManned = true;
+            return;
+        }
+        var matchHandlers = vehicle.handlers.FindAll(h => h.role.HandlingTypes.HasFlag(HandlingType.Turret) && (h.role.TurretIds.Contains(key) || h.role.TurretIds.Contains(groupKey)));
+        if (matchHandlers.Empty())
+        {
+            IsManned = false;
+            return;
+        }
+
+        IsManned = matchHandlers.All(h => h.RoleFulfilled);
+        return;
+    }
+}
