2ba888d059fe01ab457900cabec007260e02f516
oelsart <oelsart@gmail.com>
2025-11-07 13:10:18 +0900

    Set name attribute to testsuite element in JUnit custom logger initialization

--- Source/Test_Logics/CustomLogger/Logger_JUnit.cs
+++ Source/Test_Logics/CustomLogger/Logger_JUnit.cs
@@ -33,8 +33,8 @@ public class Logger_JUnit : Logger
 
     public Logger_JUnit(Config config) : base(config)
     {
+        testsuite.SetAttributeValue(NameAttribute, "Local test by DevTools");
         document.Add(testsuite);
-        testsuite.Add(new XElement("properties"));
     }
 
     public void ParseAndAdd(string message)
