29f7e35caa159ef2649bfe6182933861e775e6c7
oelsart <oelsart@gmail.com>
2025-11-15 20:56:30 +0900

    VehicleTurret_Manualにコンストラクターを追加

--- Source/VehicleMapFramework/Combat/VehicleTurret_Manual.cs
+++ Source/VehicleMapFramework/Combat/VehicleTurret_Manual.cs
@@ -6,6 +6,29 @@ namespace VehicleMapFramework;
 
 public class VehicleTurret_Manual : VehicleTurret
 {
+    /// <summary>
+    /// Init from CompProperties
+    /// </summary>
+    public VehicleTurret_Manual()
+    {
+    }
+
+    /// <summary>
+    /// Init from save file
+    /// </summary>
+    public VehicleTurret_Manual(VehiclePawn vehicle) : base(vehicle)
+    {
+    }
+
+    /// <summary>
+    /// Newly Spawned
+    /// </summary>
+    /// <param name="vehicle"></param>
+    /// <param name="reference">VehicleTurret as defined in xml</param>
+    public VehicleTurret_Manual(VehiclePawn vehicle, VehicleTurret reference) : base(vehicle, reference)
+    {
+    }
+
     public override void RecacheMannedStatus()
     {
         if (VehicleMod.settings.debug.debugShootAnyTurret)
