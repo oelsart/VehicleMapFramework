b92cff4f661ab5bd07183c958c7ead68cfaa79c6
oelsart <oelsart@gmail.com>
2025-12-02 07:23:41 +0900

    キャラバンのダイアログで車両マップを表示できるようにした

--- Source/VehicleMapFramework/Utilities/VehicleMapUIRenderer.cs
+++ Source/VehicleMapFramework/Utilities/VehicleMapUIRenderer.cs
@@ -1,6 +1,6 @@
 ﻿using SmashTools;
-using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Verse;
 
 namespace VehicleMapFramework;
@@ -11,6 +11,8 @@ public static class VehicleMapUIRenderer
     const int VEHICLE_MAP_LAYER = 31;
     
     private static Camera Camera { get; } = CreateCamera();
+    
+    private static CommandBuffer commandBuffer;
 
     private static Camera CreateCamera()
     {
@@ -28,19 +30,26 @@ public static class VehicleMapUIRenderer
         component.transform.position = new Vector3(0f, 5f, 0f);
         component.nearClipPlane = 0f;
         component.farClipPlane = 5f;
+        commandBuffer = new CommandBuffer { name = "VehicleMapDrawBuffer" };
+        component.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, commandBuffer);
         return component;
     }
     
-    public static Texture GetVehicleMapTexture(VehiclePawnWithMap vehicle, Rot4 rot, Vector2Int texSize)
+    public static Texture GetVehicleMapTexture(VehiclePawnWithMap vehicle, Rot4 rot, Vector2Int texSize,
+        Vector2? drawSize = null, Vector3? drawOffset = null)
     {
-        var mapSize = vehicle.VehicleMap.Size;
-        var maxSize = Mathf.Max(mapSize.x, mapSize.z);
+        var mapSize = vehicle.VehicleMap.Size.ToVector2();
+        var mapOrigin = new Vector3(-mapSize.x / 2f, 0f, -mapSize.y / 2f).RotatedBy(rot);
+        var proportions = drawSize ?? mapSize;
+        var offset = drawOffset ?? Vector3.zero;
+        var maxSize = Mathf.Max(proportions.x, proportions.y);
         var renderTexture = RenderTexture.GetTemporary(texSize.x, texSize.y);
         var prevCamera = Camera.current;
         Camera.gameObject.SetActive(true);
         Camera.orthographicSize = maxSize / 2f;
         Camera.targetTexture = renderTexture;
-        RenderVehicleMap(vehicle.VehicleMap, new Vector3(-maxSize + mapSize.x, 0f, -maxSize / 2f).RotatedBy(rot), rot);
+        commandBuffer.Clear();
+        RenderVehicleMap(vehicle.VehicleMap, mapOrigin + offset, rot);
         Camera.Render();
         Camera.targetTexture = null;
         Camera.gameObject.SetActive(false);
@@ -66,19 +75,20 @@ public static class VehicleMapUIRenderer
 
     private static void DrawSection(Section section, Vector3 drawPos, Rot4 rot, VehicleSectionLayerManager component)
     {
+        var rotation = Quaternion.AngleAxis(rot.AsAngle, Vector3.up);
         DrawLayerNow(section.GetLayer(typeof(SectionLayer_TerrainOnVehicle)));
         DrawLayerNow(component.GetLayer(section, typeof(SectionLayer_ThingsGeneral), rot));
         return;
         
         void DrawLayerNow(SectionLayer layer)
         {
-            var rotation = Quaternion.AngleAxis(rot.AsAngle, Vector3.up);
             for (var i = 0; i < layer.subMeshes.Count; i++)
             {
                 var subMesh = layer.subMeshes[i];
                 if (subMesh.finalized && !subMesh.disabled)
                 {
-                    Graphics.DrawMesh(subMesh.mesh, drawPos, rotation, subMesh.material, VEHICLE_MAP_LAYER);
+                    commandBuffer.DrawMesh(subMesh.mesh, Matrix4x4.TRS(drawPos, rotation, Vector3.one),
+                        subMesh.material);
                 }
             }
         }
