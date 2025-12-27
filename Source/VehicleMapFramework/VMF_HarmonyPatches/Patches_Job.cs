a0bf23a404fed4bf6a505c0d0e3f9ab01cd69c55
oelsart <oelsart@gmail.com>
2025-12-28 03:11:42 +0900

    MultiFloors導入されかつダイナミックパッチが有効時、VirtualTeleporterがDisposeされない問題の修正

--- Source/VehicleMapFramework/VMF_HarmonyPatches/Patches_Job.cs
+++ Source/VehicleMapFramework/VMF_HarmonyPatches/Patches_Job.cs
@@ -511,10 +511,10 @@ public static class Patch_Toils_Goto_GotoBuild
 //GotoCellと同じやり方でSittableOrSpotのチェック
 [HarmonyPatchCategory(EarlyPatchCore.Category)]
 [HarmonyPatch(typeof(ReservationUtility), nameof(ReservationUtility.ReserveSittableOrSpot))]
+[PatchLevel(Level.Mandatory)]
+[HarmonyBefore(ProgressionEducation.HarmonyId)]
 public static class Patch_ReservationUtility_ReserveSittableOrSpot
 {
-    [HarmonyBefore(ProgressionEducation.HarmonyId)]
-    [PatchLevel(Level.Mandatory)]
     public static bool Prefix(Pawn pawn, IntVec3 exactSittingPos, Job job, ref Map __state)
     {
         Map map;
@@ -535,7 +535,6 @@ public static class Patch_ReservationUtility_ReserveSittableOrSpot
         return exactSittingPos.InBounds(map);
     }
 
-    [PatchLevel(Level.Safe)]
     public static void Finalizer(Pawn pawn, IntVec3 exactSittingPos, Job job, Map __state, bool __result)
     {
         if (__state != null)
