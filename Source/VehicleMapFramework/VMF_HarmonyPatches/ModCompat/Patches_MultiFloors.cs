ab0537f519f250ba9252d68b64ac97d3467801b3
oelsart <oelsart@gmail.com>
2025-12-28 03:45:01 +0900

    MultiFloorsとのコンフリクトにより複製されたポーンを削除するパッチを追加

--- Source/VehicleMapFramework/VMF_HarmonyPatches/ModCompat/Patches_MultiFloors.cs
+++ Source/VehicleMapFramework/VMF_HarmonyPatches/ModCompat/Patches_MultiFloors.cs
@@ -1,19 +1,60 @@
-﻿// using System.Collections.Generic;
-// using HarmonyLib;
-//
-// namespace VehicleMapFramework.VMF_HarmonyPatches;
-//
-// [StaticConstructorOnStartupPriority(Priority.Low)]
-// internal static class Patches_MultiFloors
-// {
-//     static Patches_MultiFloors()
-//     {
-//         if (ModCompat.MultiFloors.Active)
-//         {
-//             VMF_Harmony.PatchCategory(PatchCategories.MultiFloors);
-//         }
-//     }
-// }
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+
+namespace VehicleMapFramework.VMF_HarmonyPatches;
+
+[StaticConstructorOnStartupPriority(Priority.Low)]
+internal static class Patches_MultiFloors
+{
+    static Patches_MultiFloors()
+    {
+        if (MultiFloors.Active)
+        {
+            VMF_Harmony.PatchCategory(PatchCategories.MultiFloors);
+        }
+    }
+}
+
+[HarmonyPatchCategory(PatchCategories.MultiFloors)]
+[HarmonyPatch(typeof(LoadedObjectDirectory), nameof(LoadedObjectDirectory.RegisterLoaded))]
+[PatchLevel(Level.Mandatory)]
+public static class Patch_LoadedObjectDirectory_RegisterLoaded
+{
+    public static bool Prefix(ILoadReferenceable reffable, Dictionary<string, ILoadReferenceable> ___allObjectsByLoadID)
+    {
+        if (reffable is not Pawn pawn) return true;
+        var text = "[excepted]";
+        try
+        {
+            text = reffable.GetUniqueLoadID();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        if (___allObjectsByLoadID.TryGetValue(text, out _))
+        {
+            var text2 = "[excepted]";
+            try
+            {
+                text2 = reffable.ToString();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            VMF_Log.Warning($"Pawn duplication detected. Destroying the duplicated pawn: {text2}.");
+            pawn.Destroy();
+            return false;
+        }
+
+        return true;
+    }
+}
 
 //[HarmonyPatchCategory(PatchCategories.MultiFloors)]
 //[HarmonyPatch("MultiFloors.Maps.LevelMapGenerator", "SetupMapGenerator")]
