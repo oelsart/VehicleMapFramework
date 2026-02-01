d78965713155a9d17aa502cf8ca24dd769e31ee4
oelsart <oelsart@gmail.com>
2026-02-01 16:30:00 +0900

    CompDelayedKill

---
+++ Source/VehicleMapFramework/Comps/CompProperties_DelayedKill.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Verse;
+
+namespace VehicleMapFramework;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class CompProperties_DelayedKill : CompProperties
+{
+    public int delayTicks = 480;
+
+    public EffecterDef effecterDef;
+
+    [MustTranslate]
+    public string message;
+    
+    public CompProperties_DelayedKill()
+    {
+        compClass = typeof(CompDelayedKill);
+    }
+}
