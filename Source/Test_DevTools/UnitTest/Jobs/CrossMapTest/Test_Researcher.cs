7378e72b232f0c07ab0cfb1ef077e5add552d876
oelsart <oelsart@gmail.com>
2026-05-21 16:04:57 +0900

    CrossMapWorkGiverのテストに研究(Test_Researcher)を追加 csprojのCopyToDevターゲットにおけるrobocopyの除外設定を調整

---
+++ Source/Test_DevTools/UnitTest/Jobs/CrossMapTest/Test_Researcher.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Vehicles.Testing;
+using Verse;
+
+namespace VehicleMapFramework.Test_Logics;
+
+internal class Test_Researcher(VehicleGroup group) : CrossMapWorkGiverTestBase(group)
+{
+    public override WorkGiverDef WorkGiverDef => DefDatabase<WorkGiverDef>.GetNamed("Research");
+
+    private Thing bench;
+
+    public override void SetUp()
+    {
+        base.SetUp();
+        Find.ResearchManager.SetCurrentProject(ResearchProjectDefOf.CarpetMaking);
+        var map = GroundMap;
+        bench = ThingMaker.MakeThing(ThingDefOf.SimpleResearchBench, ThingDefOf.WoodLog);
+        GenSpawn.Spawn(bench, FromRUCorner(map, 4), map);
+    }
+
+    public override void TearDown()
+    {
+        Find.ResearchManager.StopProject(ResearchProjectDefOf.CarpetMaking);
+        bench.Destroy();
+        base.TearDown();
+    }
+}
