9a94d3bfff7c2f2b761e63fedd6118f794fb8487
oelsart <oelsart@gmail.com>
2026-05-31 11:54:21 +0900

    CAI5000からの自動パッチによるエラーがあったので、ThinkNode_JobFromGotoDestMap.TryIssueJobPackageにNoJobを返させる

--- Source/VehicleMapFramework/Jobs/JobDrivers/JobDriver_GotoDestMap.cs
+++ Source/VehicleMapFramework/Jobs/JobDrivers/JobDriver_GotoDestMap.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Verse;
 using Verse.AI;
 
@@ -74,6 +73,6 @@ public class JobDriver_GotoDestMap : JobDriverAcrossMaps
 
     public class ThinkNode_JobFromGotoDestMap : ThinkNode
     {
-        public override ThinkResult TryIssueJobPackage(Pawn pawn, JobIssueParams jobParams) => throw new NotImplementedException();
+        public override ThinkResult TryIssueJobPackage(Pawn pawn, JobIssueParams jobParams) => ThinkResult.NoJob;
     }
 }
