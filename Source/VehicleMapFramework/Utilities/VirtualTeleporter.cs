4942fa1636d55e57161d5db5fadd949c90576eed
oelsart <oelsart@gmail.com>
2026-08-22 19:51:44 +0900

    VirtualTeleporterで一応DepartPositionを復元するよう変更

--- Source/VehicleMapFramework/Utilities/VirtualTeleporter.cs
+++ Source/VehicleMapFramework/Utilities/VirtualTeleporter.cs
@@ -6,46 +6,52 @@ namespace VehicleMapFramework;
 
 public readonly struct VirtualTeleporter : IDisposable
 {
-    public static readonly AccessTools.FieldRef<Thing, sbyte> mapIndexOrState = AccessTools.FieldRefAccess<Thing, sbyte>("mapIndexOrState");
-    
-    private readonly Thing _thing;
-    private readonly Map _map;
-    private readonly IntVec3 _pos = IntVec3.Invalid;
-    private readonly bool _setDepartMap;
-    private readonly Map _departMap;
-        
-    public VirtualTeleporter(Thing thing, Map map, IntVec3? c = null, bool setDepartMap = false)
-    {
-        _thing = thing;
-        if (thing is not { Spawned: true }) return;
+  public static readonly AccessTools.FieldRef<Thing, sbyte> mapIndexOrState =
+    AccessTools.FieldRefAccess<Thing, sbyte>("mapIndexOrState");
 
-        _map = thing.Map;
-        _pos = thing.Position;
-        if (map is not null)
-            mapIndexOrState(thing) = (sbyte)map.Index;
-        if (c.HasValue) thing.SetPositionDirect(c.Value);
-        _setDepartMap = setDepartMap;
-        if (_setDepartMap && thing is Pawn pawn)
-        {
-          _departMap = pawn.DepartMap;
-          pawn.DepartMap = _map;
-          if (c.HasValue) pawn.DepartPosition = _pos;
-        }
-    }
+  private readonly Thing _thing;
+  private readonly Map _map;
+  private readonly IntVec3 _pos = IntVec3.Invalid;
+  private readonly bool _setDepartMap;
+  private readonly Map _departMap;
+  private readonly IntVec3? _departPosition;
 
-    public void Dispose()
+  public VirtualTeleporter(Thing thing, Map map, IntVec3? c = null, bool setDepartMap = false)
+  {
+    _thing = thing;
+    if (thing is not { Spawned: true }) return;
+
+    _map = thing.Map;
+    _pos = thing.Position;
+    if (map is not null)
+      mapIndexOrState(thing) = (sbyte)map.Index;
+    if (c.HasValue) thing.SetPositionDirect(c.Value);
+    _setDepartMap = setDepartMap;
+    if (_setDepartMap && thing is Pawn pawn)
     {
-        if (_thing is not null)
-        {
-            if (_map is not null)
-                mapIndexOrState(_thing) = (sbyte)_map.Index;
-            if (_pos.IsValid)
-                _thing.SetPositionDirect(_pos);
-            if (_setDepartMap && _thing is Pawn pawn)
-            {
-              pawn.DepartMap = _departMap;
-              pawn.DepartPosition = null;
-            }
-        }
+      _departMap = pawn.DepartMap;
+      pawn.DepartMap = _map;
+      if (c.HasValue)
+      {
+        _departPosition = pawn.DepartPosition;
+        pawn.DepartPosition = _pos;
+      }
     }
+  }
+
+  public void Dispose()
+  {
+    if (_thing is not null)
+    {
+      if (_map is not null)
+        mapIndexOrState(_thing) = (sbyte)_map.Index;
+      if (_pos.IsValid)
+        _thing.SetPositionDirect(_pos);
+      if (_setDepartMap && _thing is Pawn pawn)
+      {
+        pawn.DepartMap = _departMap;
+        pawn.DepartPosition = _departPosition;
+      }
+    }
+  }
 }
