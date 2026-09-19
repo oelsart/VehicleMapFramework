c4f8d29d7d1019754b58ed909f3ef2d2fef74147
oelsart <oelsart@gmail.com>
2026-09-19 10:46:33 +0900

    アラートパッチの探索範囲を変更

--- Source/VehicleMapFramework/VMF_HarmonyPatches/Patches_Alert.cs
+++ Source/VehicleMapFramework/VMF_HarmonyPatches/Patches_Alert.cs
@@ -11,42 +11,56 @@ namespace VehicleMapFramework.VMF_HarmonyPatches;
 [PatchLevel(Level.Safe)]
 public static class Patch_Alert_NeedMealSource_NeedMealSource
 {
-    private static readonly FastInvokeHandler NeedMealSource = MethodInvoker.GetHandler(AccessTools.Method(typeof(Alert_NeedMealSource), "NeedMealSource"));
+  private static readonly FastInvokeHandler NeedMealSource =
+    MethodInvoker.GetHandler(AccessTools.Method(typeof(Alert_NeedMealSource), "NeedMealSource"));
 
-    public static void Postfix(Alert_NeedMealSource __instance, Map map, ref bool __result)
+  public static void Postfix(Alert_NeedMealSource __instance, Map map, ref bool __result)
+  {
+    if (!__result) return;
+
+    foreach (var map2 in map.BaseMapAndVehicleMaps(false))
     {
-        __result &= VehiclePawnWithMapCache.AllVehiclesOn(map).All(v => (bool)NeedMealSource(__instance, SingleParam.Get(v.VehicleMap)));
+      if (!(bool)NeedMealSource(__instance, SingleParam.Get(map2)))
+      {
+        __result = false;
+        return;
+      }
     }
+  }
 }
 
 [HarmonyPatch(typeof(Alert_NeedColonistBeds), nameof(Alert_NeedColonistBeds.AvailableColonistBeds))]
 [PatchLevel(Level.Sensitive)]
 public static class Patch_Alert_NeedColonistBeds_AvailableColonistBeds
 {
-    private static readonly List<Building> buildings = [];
+  private static readonly List<Building> buildings = [];
 
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+  public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+  {
+    var f_allBuildingsColonist =
+      AccessTools.Field(typeof(ListerBuildings), nameof(ListerBuildings.allBuildingsColonist));
+    
+    foreach (var instruction in instructions.MethodReplacer(CachedMethodInfo.g_Thing_Map,
+               CachedMethodInfo.m_BaseMapOrCaravan_Thing))
     {
-        foreach (var instruction in instructions.MethodReplacer(CachedMethodInfo.g_Thing_Map, CachedMethodInfo.m_BaseMapOrCaravan_Thing))
-        {
-            yield return instruction;
+      yield return instruction;
 
-            if (instruction.LoadsField(AccessTools.Field(typeof(ListerBuildings), nameof(ListerBuildings.allBuildingsColonist))))
-            {
-                yield return CodeInstruction.LoadArgument(0);
-                yield return ((Delegate)AddBuildings).Method.CallInstruction;
-            }
-        }
+      if (instruction.LoadsField(f_allBuildingsColonist))
+      {
+        yield return CodeInstruction.LoadArgument(0);
+        yield return ((Delegate)AddBuildings).Method.CallInstruction;
+      }
     }
+  }
 
-    private static List<Building> AddBuildings(List<Building> list, Map map)
-    {
-        var allVehicles = VehiclePawnWithMapCache.AllVehiclesOn(map);
-        if (allVehicles.NullOrEmpty()) return list;
-        
-        buildings.Clear();
-        buildings.AddRange(list);
-        buildings.AddRange(allVehicles.SelectMany(v => v.VehicleMap.listerBuildings.allBuildingsColonist));
-        return buildings;
-    }
+  private static List<Building> AddBuildings(List<Building> list, Map map)
+  {
+    var baseMapAndVehicleMaps = map.BaseMapAndVehicleMaps(false);
+    if (baseMapAndVehicleMaps.NullOrEmpty()) return list;
+
+    buildings.Clear();
+    buildings.AddRange(list);
+    buildings.AddRange(baseMapAndVehicleMaps.SelectMany(m => m.listerBuildings.allBuildingsColonist));
+    return buildings;
+  }
 }
